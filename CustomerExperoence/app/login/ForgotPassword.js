4a2dcbc78b11a12c4625563fefc64d5ece0af2aa
Datta Kunde <dattatraya.kunde@gmail.com>
2020-07-11 16:51:46 +0530

    added forgot pswd screen

---
+++ CustomerExperoence/app/login/ForgotPassword.js
@@ -0,0 +1,143 @@
+import {
+  Image,
+  ImageBackground,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import {MarginConstants} from '../styles/margin.constants';
+import {Colors, textColors} from '../styles/color.constants';
+import {TextSizes} from '../styles/textsize.constants';
+
+const ForgotPassword = ({navigation}) => {
+  const onSignInPress = () => {
+    navigation.navigate('SignInScreen');
+  };
+
+  const onBackPress = () => {
+    navigation.pop();
+  };
+
+  const handleEmail = text => {};
+
+  return (
+    <View style={{flex: 1}}>
+      <ImageBackground
+        resizeMode={'stretch'}
+        source={require('../images/background_inverted.png')}
+        style={{flex: 1}}>
+        <View style={styles.forgotPswdContainer}>
+          <View style={styles.navigationBar}>
+            <TouchableOpacity style={styles.backButton} onPress={onBackPress}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  color: textColors.primary,
+                  alignSelf: 'center',
+                }}>
+                Back
+              </Text>
+            </TouchableOpacity>
+            <Text
+              style={{
+                width: '100%',
+                fontSize: 20,
+                fontWeight: 'bold',
+                color: textColors.primary,
+                alignSelf: 'center',
+              }}>
+              Forgot Password?
+            </Text>
+          </View>
+
+          <Text
+            style={{
+              fontSize: 15,
+              color: textColors.primary,
+              alignSelf: 'flex-start',
+              marginTop: MarginConstants.tab3,
+              paddingHorizontal: MarginConstants.tab2,
+            }}>
+            Please enter the details below and hit Reset Password button to
+            reset your password.
+          </Text>
+          <TextInput
+            style={styles.emailInput}
+            underlineColorAndroid="transparent"
+            placeholder="Email Address"
+            placeholderTextColor="#707070"
+            autoCapitalize="none"
+            onChangeText={handleEmail}
+          />
+          <TextInput
+            style={styles.passwordInput}
+            underlineColorAndroid="transparent"
+            placeholder="Company Code"
+            placeholderTextColor="#707070"
+            autoCapitalize="none"
+            onChangeText={handleEmail}
+          />
+
+          <TouchableOpacity style={styles.nextButton} onPress={onSignInPress}>
+            <Text styele={styles.nextText}> Reset Password </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default ForgotPassword;
+
+const styles = StyleSheet.create({
+  forgotPswdContainer: {
+    alignItems: 'center',
+    width: '100%',
+  },
+  navigationBar: {
+    width: '100%',
+    height: 50,
+    flexDirection: 'row',
+    backgroundColor: Colors.accent,
+    alignItems: 'flex-start',
+  },
+
+  backButton: {
+    color: textColors.primary,
+    fontSize: Platform.isPad ? TextSizes.largeText : TextSizes.largeText,
+  },
+  emailInput: {
+    width: '90%',
+    marginTop: MarginConstants.tab3,
+    paddingHorizontal: MarginConstants.tab2,
+    textAlign: 'left',
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+  },
+  passwordInput: {
+    width: '90%',
+    marginTop: MarginConstants.tab2,
+    paddingHorizontal: MarginConstants.tab2,
+    textAlign: 'left',
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+  },
+  nextButton: {
+    width: '90%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: MarginConstants.tab4,
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+  },
+  nextText: {
+    alignSelf: 'center',
+    color: textColors.primary,
+  },
+});
