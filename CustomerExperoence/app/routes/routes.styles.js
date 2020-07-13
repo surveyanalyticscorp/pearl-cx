44957a19fe40ecc8dba8717dca4214d8fc5b93ed
Datta Kunde <dattatraya.kunde@gmail.com>
2020-07-13 18:30:36 +0530

    route styles added.

---
+++ CustomerExperoence/app/routes/routes.styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  topTabBarStyle: {
+    indicatorStyle: {backgroundColor: '#FF0000'},
+    scrollEnabled: true,
+    labelStyle: {color: '#000000', fontSize: 12},
+    tabStyle: {width: 150},
+    style: {backgroundColor: '#FFFFFF'},
+  },
+});
