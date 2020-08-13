21f2f517f3e558cc22e1c6bcbcabede23b6cb428
Nehal-29 <nehal.sanklecha@questionpro.com>
2020-08-13 11:19:51 +0530

    directory name changed

--- CustomeExperience/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import CxApp from './app/index';
-
-AppRegistry.registerComponent(appName, () => CxApp);
