c7b7a02abbf206a105eef2587c9fb3f4139ce8c1
Nehal-29 <nehal.sanklecha@questionpro.com>
2020-08-13 12:56:17 +0530

    android project renamed

--- CustomerExperience/android/app/src/main/java/com/customerexperoence/MainActivity.java
+++ CustomerExperience/android/app/src/main/java/com/customerexperoence/MainActivity.java
@@ -1,4 +1,4 @@
-package com.customerexperoence;
+package com.customerexperience;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "CustomerExperoence";
+    return "CustomerExperience";
   }
 }
--- CustomerExperience/ios/CustomerExperienceTests/CustomerExperienceTests.m
+++ CustomerExperience/ios/CustomerExperienceTests/CustomerExperienceTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CustomerExperoenceTests : XCTestCase
+@interface CustomerExperienceTests : XCTestCase
 
 @end
 
-@implementation CustomerExperoenceTests
+@implementation CustomerExperienceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
