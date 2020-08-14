572a01c8896a6aed43025b74841202ece3b9f91b
Nehal-29 <nehal.sanklecha@questionpro.com>
2020-08-14 16:18:39 +0530

    react-native-debugger integrated

--- CustomerExperience/app/redux/store/store.js
+++ CustomerExperience/app/redux/store/store.js
@@ -1,17 +1,24 @@
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 import rootReducer from '../reducer/index.reducer';
 import {createLogger} from 'redux-logger';
 
 import createSagaMiddleware from 'redux-saga';
 import {rootSaga} from '../sagas/index';
 
+const enhancerList = [];
+const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
+if (typeof devToolsExtension === 'function') {
+  enhancerList.push(devToolsExtension());
+}
+
 const sagaMiddleware = createSagaMiddleware();
+const composedEnhancer = compose(
+  applyMiddleware(sagaMiddleware, createLogger()),
+  ...enhancerList,
+);
 
 // Redux: Store
-const store = createStore(
-  rootReducer,
-  applyMiddleware(sagaMiddleware, createLogger()),
-);
+const store = createStore(rootReducer, {}, composedEnhancer);
 
 // Middleware: Redux Saga
 sagaMiddleware.run(rootSaga);
