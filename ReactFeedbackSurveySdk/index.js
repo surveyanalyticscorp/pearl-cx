82abd5f87a49167135f04f0612a72e62f629e30a
Datta Kunde <dattatraya.kunde@gmail.com>
2022-03-02 15:24:52 +0530

    SDK Code cleanup

--- ReactFeedbackSurveySdk/index.js
+++ ReactFeedbackSurveySdk/index.js
@@ -1,5 +1,4 @@
-import {QpTextView} from "./src/TextView";
 import {initQp} from './src/cx_lib/QuestionProCx'
 import {QpFeedbackSurvey} from './src/cx_lib/components/QpFeedbackSurvey'
 
-export {QpTextView, initQp, QpFeedbackSurvey}
+export {initQp, QpFeedbackSurvey}
