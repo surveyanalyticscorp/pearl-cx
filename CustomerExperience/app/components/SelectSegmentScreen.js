5126df3eaf3e2e369eacd4c18d6b10b8a9049b90
Dattakunde <datta.kunde@questionpro.com>
2022-12-22 11:52:35 +0530

    SelectSegmentScreen refactoring.

--- CustomerExperience/app/components/SelectSegmentScreen.js
+++ CustomerExperience/app/components/SelectSegmentScreen.js
@@ -49,53 +49,11 @@ const SelectSegmentScreen = (props) => {
       navigation.goBack();
     }
   };
-  // console.log('CURRENT_SEGMENT_ID', currentSegmentId);
-  // console.log('CURRENT_SEGMENT_LIST', JSON.stringify(segmentList));
-
+  
   useEffect(() => {
     getSegmentData();
     // getSegmentDataFromLocal();
   }, [pageOffset]);
-
-  // useEffect(() => {
-  //   setLoading(false);
-  // }, [segmentList]);
-  // useCallback(() => {
-  //   setSegmentList_((prevState) => [
-  //     ...new Set([...prevState, ...(segmentDetails.segments ?? [])]),
-  //   ]);
-  // }, [segmentDetails.pageOffset]);
-
-  // useEffect(() => {
-  // let data = pageOffset === 0 ? [] : [...segmentList_];
-  // data = [...data, ...segmentDetails.segments];
-
-  // setSegmentList_([new Set(data)]);
-  // setLoading(false);
-  // if (pageOffset === 0) {
-  //   // setSegmentList(segmentDetails.segments);
-  // } else {
-  //   let list = [...segmentList, ...segmentDetails.segments];
-  //   let uniqueList = [
-  //     ...new Map(list.map((item) => [item['segmentID'], item])).values(),
-  //   ];
-  // setSegmentList(uniqueList);
-  // setSegmentList([...new Set(list)]);
-  // let data = pageOffset === 0 ? [] : [...segmentList];
-  // data = [...data, ...segmentDetails.segments];
-
-  // setSegmentList([new Set(data)]);
-  // }
-  // }, [segmentDetails]);
-
-  // const loadMoreData = useCallback(() => {
-  //   console.log('Segment_API_CALL_LOADMORE');
-
-  //   if (segmentList_.length < segmentDetails.count) {
-  //     setPageOffset((state) => state + 1);
-  //     getSegmentData(pageOffset);
-  //   }
-  // }, []);
 
   const loadMoreData = () => {
     // console.log('LOAD_MORE', segmentList.length, segmentDetails.count);
@@ -165,62 +123,53 @@ const SelectSegmentScreen = (props) => {
     );
   };
 
+  const renderRow = ({item, index}) => {
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => handleSegmentSelectionAction(item)}>
+        <View style={styles.row}>
+          <Text style={styles.title}>{item.segmentName}</Text>
+          {currentSegmentId === item.segmentID ? (
+            <IonIcons
+              style={{marginHorizontal: MarginConstants.halfTab}}
+              name={'checkmark'}
+              size={20}
+              color={Colors.filterIconColor}
+            />
+          ) : (
+            <View />
+          )}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
+
+  const renderSegmentSearch = () => {
+    return (
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search..."
+        onChangeText={(text) => {
+          console.log(text);
+          // if (text) {
+          //   setFilteredList((state) =>
+          //     state.filter((item) =>
+          //       item.segmentName.toLowerCase().includes(text.toLowerCase()),
+          //     ),
+          //   );
+          // } else {
+          //   setFilteredList(data);
+          // }
+        }}
+      />
+    );
+  };
+
   const globalSelectSegment = (props) => {
-    // const [filteredList, setFilteredList] = useState(data);
-    // const [selectedIndex, setSelectedIndex] = useState(props.selectedIndex ?? 0);
-
-    // const flatListRef = React.createRef();
-    // const scrollPositionRef = React.createRef();
-    const renderRow = ({item, index}) => {
-      return (
-        <TouchableWithoutFeedback
-          onPress={() => handleSegmentSelectionAction(item)}>
-          <View style={styles.row}>
-            <Text style={styles.title}>{item.segmentName}</Text>
-            {currentSegmentId === item.segmentID ? (
-              <IonIcons
-                style={{marginHorizontal: MarginConstants.halfTab}}
-                name={'checkmark'}
-                size={20}
-                color={Colors.filterIconColor}
-              />
-            ) : (
-              <View />
-            )}
-          </View>
-        </TouchableWithoutFeedback>
-      );
-    };
-
-    // const handleOnPress = (item, index) => {
-    //   handleOnPress(item);
-    // };
 
     console.log('Segment_API_CALL');
-
-    const renderSegmentSearch = () => {
-      return (
-        <TextInput
-          style={styles.searchInput}
-          placeholder="Search..."
-          onChangeText={(text) => {
-            console.log(text);
-            // if (text) {
-            //   setFilteredList((state) =>
-            //     state.filter((item) =>
-            //       item.segmentName.toLowerCase().includes(text.toLowerCase()),
-            //     ),
-            //   );
-            // } else {
-            //   setFilteredList(data);
-            // }
-          }}
-        />
-      );
-    };
-
     return (
-      // <View style={styles.innerContainer}>
       <FlatList
         // ref={flatListRef}
         style={styles.flatList}
@@ -234,17 +183,7 @@ const SelectSegmentScreen = (props) => {
         // ListFooterComponent={isLoading && <RenderSpinner />}
         refreshing={false}
         extraData={segmentList}
-        // onViewableItemsChanged={() => {
-        //   flatListRef.current.scrollToOffset({
-        //     animated: false,
-        //     offset: scrollPositionRef.current,
-        //   });
-        // }}
-        // onScroll={(event) =>
-        //   (scrollPositionRef.current = event.nativeEvent.contentOffset.y)
-        // }
       />
-      // </View>
     );
   };
 
@@ -263,7 +202,7 @@ const SelectSegmentScreen = (props) => {
           <Text style={styles.headerText}>{'Select Segment'}</Text>
           <CloseButton color={Colors.filterIconColor} />
         </View>
-        {/* <RenderSelectSegment /> */}
+      
         {globalSelectSegment()}
       </View>
     </View>
