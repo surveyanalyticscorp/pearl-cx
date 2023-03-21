40e6c287a7df081f15f602cf64132f3b6bccdf0f
Mehedi Hasan <mehedi.prolific@gmail.com>
2023-03-21 18:09:49 +0600

    feat: offline search for assignee/owner list

--- CustomerExperience/app/components/closedloop/takeaction/SelectTicketOwner.js
+++ CustomerExperience/app/components/closedloop/takeaction/SelectTicketOwner.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {
   View,
   TouchableWithoutFeedback,
   Text,
   StyleSheet,
   FlatList,
+  TextInput,
 } from 'react-native';
 import {Colors} from '../../../styles/color.constants';
 import {FontFamily} from '../../../styles/font.constants';
@@ -13,11 +14,24 @@ import {TextSizes} from '../../../styles/textsize.constants';
 // import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import {listItemSeparator} from '../../../routes/CommonScreen';
+import {PaddingConstants} from '../../../styles/padding.constants';
+import StringUtils from '../../../Utils/StringUtils';
+import {NoItemsFound} from '../../../routes/CommonScreen';
 
 const SelectTicketOwner = (props) => {
   const [data, setData] = useState(props.data);
   const [selectedIndex, setSelectedIndex] = useState(props.selectedIndex);
+  const textInputRef = useRef();
 
+  function filterOwnerList(text) {
+    if (StringUtils.isEmpty(text)) {
+      setData(props.data);
+    } else {
+      setData((prevState) =>
+        prevState.filter((item) => item.ownerName.includes(text)),
+      );
+    }
+  }
   const renderRow = ({item, index}) => {
     return (
       <TouchableWithoutFeedback
@@ -47,12 +61,25 @@ const SelectTicketOwner = (props) => {
 
   return (
     <View style={styles.container}>
+      <TextInput
+        ref={textInputRef}
+        defaultValue={''}
+        style={styles.search}
+        placeholder="Search assignee/Owner name here..."
+        returnKeyType={'search'}
+        onChangeText={(text) => {
+          filterOwnerList(text);
+        }}
+      />
       <FlatList
         style={styles.flatList}
         data={data}
         keyExtractor={(item, index) => index.toString()}
         renderItem={renderRow}
         ItemSeparatorComponent={listItemSeparator}
+        ListEmptyComponent={
+          <NoItemsFound>No Assignee/Owner found</NoItemsFound>
+        }
       />
     </View>
   );
@@ -78,5 +105,11 @@ const styles = StyleSheet.create({
     fontSize: TextSizes.secondary,
     marginStart: MarginConstants.halfTab,
     color: Colors.filterIconColor,
+  },
+  search: {
+    padding: PaddingConstants.halfTab,
+    margin: MarginConstants.halfTab,
+    borderColor: Colors.filterIconColor,
+    borderBottomWidth: 0.5,
   },
 });
