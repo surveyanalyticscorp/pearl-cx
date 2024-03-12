84f0c38b94b8d910a53d03717cf6eb39a206e1b3
Mehedi Hasan <mehedi.hasan@questionpro.com>
2024-03-12 10:17:58 +0600

    refactor: LegendContainer

--- CustomerExperience/app/widgets/dashboardWidget/LegendScoreView.js
+++ CustomerExperience/app/widgets/dashboardWidget/LegendScoreView.js
@@ -1,54 +1,55 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {dashboardStyles} from '../../components/dashboard/dashboard.style';
 import {FontWeight} from '../../styles/font.constants';
 import {baseTextStyles} from '../../styles/text.styles';
 import StringUtils from '../../Utils/StringUtils';
+import TextLabel from '../TextLabel/TextLabel';
+
+const LegendContainer = ({justifyContent = 'flex-start', children}) => {
+  return (
+    <View style={{justifyContent: justifyContent, ...styles.legendContainer}}>
+      {children}
+    </View>
+  );
+};
+
 const LegendScoreView = ({title, count, percentage, backgroundColor}) => {
   return (
     <View style={dashboardStyles.legendItemView}>
-      <View
-        style={{
-          flex: 2,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-        }}>
+      <LegendContainer>
         <View
           style={{
             ...dashboardStyles.legendIcon,
             backgroundColor: backgroundColor,
           }}
         />
-        <Text
-          style={{
-            ...baseTextStyles.secondaryRegularText,
-            fontWeight: FontWeight.bold,
-          }}>
-          {title ?? ''}
-        </Text>
-      </View>
-      <View
-        style={{
-          flex: 2,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-        }}>
-        <Text
-          style={{
-            ...baseTextStyles.secondaryRegularText,
-            fontWeight: FontWeight.bold,
-          }}>
-          {count}
-        </Text>
-
-        <Text style={baseTextStyles.secondaryRegularText}>
-          {`(${StringUtils.floatTo2DecimalPointString(percentage)}%)`}
-        </Text>
-      </View>
+        <TextLabel
+          text={title ?? ''}
+          baseTextStyle={baseTextStyles.secondaryRegularText}
+          fontWeight={FontWeight.bold}
+        />
+      </LegendContainer>
+      <LegendContainer justifyContent="flex-end">
+        <TextLabel
+          text={count ?? ''}
+          baseTextStyle={baseTextStyles.secondaryRegularText}
+          fontWeight={FontWeight.bold}
+        />
+        <TextLabel
+          text={`(${StringUtils.floatTo2DecimalPointString(percentage)}%)`}
+          baseTextStyle={baseTextStyles.secondaryRegularText}
+        />
+      </LegendContainer>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  legendContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 export default LegendScoreView;
