d6fd3a019fab05a4a119cbe064c484fb4ad96b6a
Mehedi Hasan <mehedi.hasan@questionpro.com>
2025-03-03 12:39:38 +0600

    Removed ReactFeedbackSurveySdk directory

--- ReactFeedbackSurveySdk/index.js
+++
@@ -1,5 +0,0 @@
-import {QpTextView} from "./src/TextView";
-import {initQp} from './src/cx_lib/QuestionProCx'
-import {QpFeedbackSurvey} from './src/cx_lib/components/QpFeedbackSurvey'
-
-export {QpTextView, initQp, QpFeedbackSurvey}
