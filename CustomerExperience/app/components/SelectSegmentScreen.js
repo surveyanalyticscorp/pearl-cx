ee438b50962a7097bc773ac81d3394ca02ddee90
Mehedi Hasan <mehedi.hasan@questionpro.com>
2025-06-23 15:47:39 +0600

    style: Add padding to SelectSegmentScreen for improved layout consistency

--- CustomerExperience/app/components/SelectSegmentScreen.js
+++ CustomerExperience/app/components/SelectSegmentScreen.js
@@ -239,6 +239,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    paddingStart: PaddingConstants.tab1_2x,
   },
   row: {
     flexDirection: 'row',
