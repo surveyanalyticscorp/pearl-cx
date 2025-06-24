c6c66b71c5f869c41b9d5db2fea3c7518d20db81
Mehedi Hasan <mehedi.hasan@questionpro.com>
2025-06-24 17:13:08 +0600

    Segment Selector: fixed font issue

--- CustomerExperience/app/components/SegmentText.js
+++ CustomerExperience/app/components/SegmentText.js
@@ -3,6 +3,7 @@ import {View, Text, StyleSheet} from 'react-native';
 import {TextSizes} from '../styles/textsize.constants';
 import {Colors} from '../styles/color.constants';
 import {MarginConstants} from '../styles/margin.constants';
+import {FontFamily} from '../styles/font.constants';
 
 const SegmentText = ({screenName, segmentName}) => {
   return (
@@ -22,12 +23,14 @@ const SegmentText = ({screenName, segmentName}) => {
 
 const styles = StyleSheet.create({
   appbarTitle: {
+    fontFamily: FontFamily.regular,
     fontSize: TextSizes.primary,
     color: Colors.white,
     marginEnd: MarginConstants.tab3,
   },
 
   appbarSegmentName: {
+    fontFamily: FontFamily.regular,
     fontSize: TextSizes.secondary,
     color: Colors.white,
     marginEnd: MarginConstants.tab3,
