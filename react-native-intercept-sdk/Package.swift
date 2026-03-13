9c48488c397e9fd30ecc390155b319964b4fc9e4
Datta Kunde <datta.kunde@questionpro.com>
2026-03-13 16:55:55 +0530

    Bump android-cx dependency to 2.2.6
    Update QuestionPro Android CX SDK dependency from 2.2.4 to 2.2.6 in react-native-intercept-sdk/android/build.gradle to use the newer SDK release.

--- react-native-intercept-sdk/Package.swift
+++ react-native-intercept-sdk/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/surveyanalyticscorp/ios-cx.git", from: "2.2.6")
+        .package(url: "https://github.com/surveyanalyticscorp/ios-cx.git", from: "2.2.7")
     ],
     targets: [
         .target(
